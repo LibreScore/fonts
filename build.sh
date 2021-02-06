1674a9741016616f5b62311e37184c0d247acf49
Xmader <xmader@outlook.com>
2021-02-06 03:57:40 -0500

    feat: build subset fonts

--- build.sh
+++
@@ -1,8 +0,0 @@
-
-pyftsubset SourceHanSansCN-Regular.woff2 \
-    --output-file="SourceHanSans.woff2" \
-    --flavor=woff2 \
-    --symbol-cmap \
-    --layout-features='*' \
-    --notdef-glyph --notdef-outline --recommended-glyphs \
-    --unicodes="U+1100-11FF,U+2E3A-33FF,U+4E00-9FFF,U+AC00-D7FF,U+F900-FFFF"
---
+++ src/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FLAGS=(
+    --flavor=woff2
+    --symbol-cmap
+    --layout-features='*'
+    --notdef-glyph --notdef-outline --recommended-glyphs
+)
+
+# install tools: https://github.com/fonttools/fonttools
+
+pyftsubset SourceHanSansCN-Regular.woff2 \
+    --output-file="../CNJP.woff2" \
+    --unicodes="U+2E3A-33FF,U+4E00-9FFF,U+F900-FAFF,U+FE10-FE19,U+FE30-FE6B,U+FF00-FFEE" \
+    ${FLAGS[@]}
+
+pyftsubset SourceHanSansKR-Regular.woff2 \
+    --output-file="../KR.woff2" \
+    --unicodes="U+1100-11FF,U+AC00-D7FF,U+3130-318F" \
+    ${FLAGS[@]}
